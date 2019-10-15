ffa80556631c054437487dfe03c553579ff79865
dannyseymour <daniel@discordia.io>
2019-10-14 22:53:14 -0600

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/dicewareservice/DicewareServiceApplication.java
@@ -0,0 +1,28 @@
+package edu.cnm.deepdive.dicewareservice;
+
+import java.security.SecureRandom;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import java.util.ResourceBundle;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DicewareServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DicewareServiceApplication.class, args);
+  }
+  @Bean
+  public Random random(){
+    return new SecureRandom();
+  }
+
+
+  @Bean
+  public ResourceBundle bundle(){
+    return ResourceBundle.getBundle("wordlist");
+  }
+}
